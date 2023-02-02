e136ea011b46ee26d0060684d304fefb1681dc90
Milindsm <milu.mangale@gmail.com>
2023-02-02 16:18:28 +0530

    Dom manipulation for title and add-item

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("This is My forst Git")
-console.log("Update")
+//GET ELEMENT BY ID
+var headerTitle = document.getElementById('header-title');
+headerTitle.style.borderBottom="solid 3px #000";
+
+var header = document.getElementById('main-header');
+header.style.borderBottom="solid 3px green";
+
+//GET EMENET BY CLASS NAME
+
+var tt = document.getElementById('add-Item');
+tt.style.fontWeight ="bold";
+tt.style.color ="green";

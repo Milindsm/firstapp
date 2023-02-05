8314bb78f4c49aba2bad7e8f756a7f77a21f34a8
Milindsm <milu.mangale@gmail.com>
2023-02-05 18:52:47 +0530

    delte from UI as well as LocalStorage

--- Bookingapp.js
+++ Bookingapp.js
@@ -5,6 +5,8 @@ const emailInput = document.querySelector('#email');
 const userList = document.querySelector('#users');
 
 
+
+
 myform.addEventListener('submit', onsubmit);
 function onsubmit(e){
     e.preventDefault();
@@ -15,7 +17,7 @@ function onsubmit(e){
     }else{
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(`${nameInput.value}:${emailInput.value}`));
-        userList.appendChild(li);
+        //userList.appendChild(li);
 
         let name = nameInput.value;
         let email = emailInput.value;
@@ -24,9 +26,43 @@ function onsubmit(e){
             name,
             email
         }
+        var dltbtn = document.createElement('dltbtn');
 
+    //add classes to delete button
+    dltbtn.className = 'dltbtn';
+    //append text node
+    dltbtn.appendChild(document.createTextNode('Delete'));
+    //appende button to li
+    li.appendChild(dltbtn);
+
+    
+    //event for delete
+    userList.addEventListener('click',removeItem);
+    
+    
+
+    function removeItem(e){
+        if (e.target.classList.contains('dltbtn')){
+
+            if(confirm('Are you sure?')){
+                
+                var li  = e.target.parentElement;
+                userList.removeChild(li);
+                localStorage.removeItem(emailInput.value);
+                }
+            
+        }
         
+    }
+
+    userList.appendChild(li);
+    
+     
        localStorage.setItem(emailInput.value,JSON.stringify(userdetails));
+       
+       
+       
+
        
         
         
